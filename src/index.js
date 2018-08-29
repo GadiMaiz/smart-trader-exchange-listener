137c93fe93b38a6567e4ee8a6b7ec15ac885d4e3
Udi Ilan <udi.ilan@bitmain.com>
2018-08-29 15:50:12 +0300

    Merge changes in VS code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 
 // Set process name
-<<<<<<< HEAD
 process.title = ['Smart Trade Exchange Listener'];
-=======
-process.title = ['']; // TO-DO: change to your process real name.
->>>>>>> 7b9fed19f8653f9322e3435f1f2abb5df1dd953b
 
 import server from 'server';
 server.start();
