9440b9706ebf060fefa4843150241d215f028a9e
Udi Ilan <udi.ilan@bitmain.com>
2018-09-16 16:32:16 +0300

    Get Kafka parameters from command line

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+import logger from 'logger';
+import argv from 'optimist';
+import {Producer, Client} from 'kafka-node';
+import bitstamp_orderbook from 'orderbook/bitstamp_orderbook';
 
-// Set process name
 process.title = ['Smart Trade Exchange Listener'];
 
-/*import server from 'server';
-server.start();*/
-
-import argv from 'optimist';
-var kafka_ip = argv.kafka_ip;
-
-import {Producer, Client} from 'kafka-node';
-//import Client from 'kafka-node';
-
-let client = new Client('localhost:2181');
+let kafka_ip = argv.kafka_ip || 'localhost';
+let kafka_port = argv.kafka_port || '2181';
+let client = new Client(kafka_ip + ':' + kafka_port);
 let producer = new Producer(client);
 let producer_ready = false;
 
@@ -36,17 +32,14 @@ class orderbook_listener
         if (this.orderbook && producer_ready)
         {
             let curr_orderbook = this.orderbook.get_orderbook(10);
-            //console.log(JSON.stringify(curr_orderbook));
             producer.send([{topic: 'orderbook_bitstamp_btc_usd', partition: 0, messages: [JSON.stringify(curr_orderbook)],
                             attributes: 0}], (err, result) => {});
         }
     }
 }
 
-import bitstamp_orderbook from 'orderbook/bitstamp_orderbook';
-
 let bitstamp_listener = new orderbook_listener(null);
-var bitstampOrderbook = new bitstamp_orderbook(bitstamp_listener);
+let bitstampOrderbook = new bitstamp_orderbook(bitstamp_listener);
 bitstamp_listener.set_listener(bitstampOrderbook.orderbook_manager);
 
 bitstampOrderbook.bind_all_channels();
