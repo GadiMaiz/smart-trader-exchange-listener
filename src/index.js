33360f33174c961b59f5b235182f5b27dbb64209
Udi Ilan <udi.ilan@bitmain.com>
2018-09-20 15:55:06 +0300

    Fix formatting. Remove unuseable overloading.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import logger from 'logger';
 import argv from 'optimist';
-import {Producer, Client} from 'kafka-node';
+import { Producer, Client } from 'kafka-node';
 import bitstamp_orderbook from 'orderbook/bitstamp_orderbook';
+import bitfinex_orderbook from 'orderbook/bitfinex_orderbook';
 
 process.title = ['Smart Trade Exchange Listener'];
 
@@ -12,30 +13,27 @@ let producer = new Producer(client);
 let producer_ready = false;
 
 producer.on('ready', () => {
-    producer_ready = true;
+  producer_ready = true;
 });
 
-class orderbook_listener
-{
-    constructor(orderbook)
-    {
-        this.set_listener(orderbook);
-    }
+class orderbook_listener {
+  constructor(orderbook) {
+    this.set_listener(orderbook);
+  }
 
-    set_listener(orderbook)
-    {
-        this.orderbook = orderbook;
-    }
+  set_listener(orderbook) {
+    this.orderbook = orderbook;
+  }
 
-    orderbook_changed()
-    {
-        if (this.orderbook && producer_ready)
-        {
-            let curr_orderbook = this.orderbook.get_orderbook(10);
-            producer.send([{topic: 'orderbook_bitstamp_btc_usd', partition: 0, messages: [JSON.stringify(curr_orderbook)],
-                            attributes: 0}], (err, result) => {});
-        }
+  orderbook_changed() {
+    if (this.orderbook && producer_ready) {
+      let curr_orderbook = this.orderbook.get_orderbook(10);
+      producer.send([{
+        topic: 'orderbook_bitstamp_btc_usd', partition: 0, messages: [JSON.stringify(curr_orderbook)],
+        attributes: 0
+      }], (err, result) => { });
     }
+  }
 }
 
 let bitstamp_listener = new orderbook_listener(null);
@@ -44,10 +42,8 @@ bitstamp_listener.set_listener(bitstampOrderbook.orderbook_manager);
 
 bitstampOrderbook.bind_all_channels();
 
-import bitfinex_orderbook from 'orderbook/bitfinex_orderbook';
-
 let bitfinex_listener = new orderbook_listener(null);
-var bitfinexOrderbook = new bitfinex_orderbook(bitfinex_listener);
+let bitfinexOrderbook = new bitfinex_orderbook(bitfinex_listener);
 bitfinex_listener.set_listener(bitfinexOrderbook.orderbook_manager);
 
 bitfinex_orderbook.init();
--- src/orderbook/bitstamp_orderbook.js
+++ src/orderbook/bitstamp_orderbook.js
@@ -4,48 +4,48 @@ const pusher = new Pusher('de504dc5763aeef9ff52');
 
 class bitstamp_orderbook
 {
-    constructor(orderbook_listener)
-    {
-        this.reset_timestamp = 0;
-        this.ordersChannel = pusher.subscribe('live_orders');
-        this.orderBookChannel = pusher.subscribe('order_book');
-        this.orderDiffBookChannel = pusher.subscribe('diff_order_book');
-        this.orderbook_manager = new orderbook_manager(orderbook_listener);
-    }
+  constructor(orderbook_listener)
+  {
+    this.reset_timestamp = 0;
+    this.ordersChannel = pusher.subscribe('live_orders');
+    this.orderBookChannel = pusher.subscribe('order_book');
+    this.orderDiffBookChannel = pusher.subscribe('diff_order_book');
+    this.orderbook_manager = new orderbook_manager(orderbook_listener);
+  }
 
-    normalize_order(data)
-    {
-        let new_order = {price: data.price, size: data.amount, 
-                         type: data.order_type == 0 ? 'bids' : 'asks',
-                         exchange_id: data.id.toString(),
-                         source: 'Bitstamp'};
-        return new_order;
-    }
+  normalize_order(data)
+  {
+    let new_order = { price: data.price, size: data.amount,
+      type: data.order_type == 0 ? 'bids' : 'asks',
+      exchange_id: data.id.toString(),
+      source: 'Bitstamp' };
+    return new_order;
+  }
 
-    normalize_orderbook_order(order, type)
-    {
+  normalize_orderbook_order(order, type)
+  {
 
-        return {price: parseFloat(order[0]), size: parseFloat(order[1]), type: type, 
-                exchange_id: order.length > 2 ? order[2] : null, source: 'Bitstamp'};
-    }
+    return { price: parseFloat(order[0]), size: parseFloat(order[1]), type: type,
+      exchange_id: order.length > 2 ? order[2] : null, source: 'Bitstamp' };
+  }
 
-    bind_channel(channel_name, cb)
-    {
-        this.ordersChannel.bind(channel_name, function (data) {
-            cb(channel_name, data);
-        });    
-    }
+  bind_channel(channel_name, cb)
+  {
+    this.ordersChannel.bind(channel_name, function (data) {
+      cb(channel_name, data);
+    });
+  }
 
-    order_callback(channel_name, order)
-    {
-        //console.log(channel_name, order);
-    }
+  order_callback(channel_name, order)
+  {
+    // console.log(channel_name, order);
+  }
 
-    bind_all_channels()
-    {
-        var orderbook = this.orderbook_manager;
-        let normalize = this.normalize_order;
-        /*this.ordersChannel.bind('order_created', function (data) {
+  bind_all_channels()
+  {
+    let orderbook = this.orderbook_manager;
+    let normalize = this.normalize_order;
+    /* this.ordersChannel.bind('order_created', function (data) {
             //if (parseInt(data.datetime) > this.reset_timestamp)
             {
                 console.log(new Date().getTime(), 'order added', JSON.stringify(data));
@@ -67,85 +67,85 @@ class bitstamp_orderbook
                 orderbook.change_order(normalize(data));
             }
         });*/
-        this.orderBookChannel.bind('data', data =>
+    this.orderBookChannel.bind('data', data =>
+    {
+      const order_types = ['asks', 'bids'];
+      // console.log(JSON.stringify(data.bids[0]));
+      // console.log(new Date().getTime(), 'orderbook', JSON.stringify(data));
+      this.orderbook_manager.clear_orderbook();
+      for (let curr_type_index in order_types)
+      {
+        for (let order_index in data[order_types[curr_type_index]])
         {
-            const order_types = ['asks', 'bids'];
-            //console.log(JSON.stringify(data.bids[0]));
-            //console.log(new Date().getTime(), 'orderbook', JSON.stringify(data));
-            this.orderbook_manager.clear_orderbook();
-            for (let curr_type_index in order_types)
-            {
-                for (let order_index in data[order_types[curr_type_index]])
-                {
-                    this.orderbook_manager.add_order(
-                        this.normalize_orderbook_order(data[order_types[curr_type_index]][order_index],
-                            order_types[curr_type_index]));
-                }
-            }
-            this.orderbook_manager.notify_orderbook_changed();
-        });
-        /*this.orderDiffBookChannel.bind('data', data =>
+          this.orderbook_manager.add_order(
+            this.normalize_orderbook_order(data[order_types[curr_type_index]][order_index],
+              order_types[curr_type_index]));
+        }
+      }
+      this.orderbook_manager.notify_orderbook_changed();
+    });
+    /* this.orderDiffBookChannel.bind('data', data =>
         {
             //console.log(new Date().getTime(), 'orderbook diff', data);
         })*/
-    }
+  }
 
-    reset_orderbook(bitstamp_orderbook_instance)
-    {
-        const url = "https://www.bitstamp.net/api/v2/order_book/btcusd/?group=2";
-        let orderbook_manager = bitstamp_orderbook_instance.orderbook_manager;
-        const get_orderbook = async url => {
-            try 
+  reset_orderbook(bitstamp_orderbook_instance)
+  {
+    const url = 'https://www.bitstamp.net/api/v2/order_book/btcusd/?group=2';
+    let orderbook_manager = bitstamp_orderbook_instance.orderbook_manager;
+    const get_orderbook = async url => {
+      try 
+      {
+        const response = await fetch(url);
+        const orderbook = await response.json();
+        bitstamp_orderbook_instance.reset_timestamp = orderbook.timestamp;
+        orderbook_manager.clear_orderbook();
+        let order_types = ['bids', 'asks'];
+        for (let type_index in order_types)
+        {
+          let orderbook_counter = 0;
+          for (let curr_bid in orderbook[order_types[type_index]])
+          {  
+            orderbook_counter++;
+            orderbook_manager.add_order(bitstamp_orderbook_instance.normalize_orderbook_order(
+              orderbook[order_types[type_index]][curr_bid], 
+              order_types[type_index]));
+            if (orderbook_counter > 9)
             {
-                const response = await fetch(url);
-                const orderbook = await response.json();
-                bitstamp_orderbook_instance.reset_timestamp = orderbook.timestamp;
-                orderbook_manager.clear_orderbook();
-                let order_types = ['bids', 'asks'];
-                for (let type_index in order_types)
-                {
-                    let orderbook_counter = 0;
-                    for (let curr_bid in orderbook[order_types[type_index]])
-                    {  
-                        orderbook_counter++;
-                        orderbook_manager.add_order(bitstamp_orderbook_instance.normalize_orderbook_order(
-                            orderbook[order_types[type_index]][curr_bid], 
-                                                    order_types[type_index]));
-                        if (orderbook_counter > 9)
-                        {
-                            break;
-                        }
-                    }
-                }
-            } 
-            catch (error) {
-              console.log(error);
+              break;
             }
-          };
-          get_orderbook(url);
-    }
+          }
+        }
+      } 
+      catch (error) {
+        console.log(error);
+      }
+    };
+    get_orderbook(url);
+  }
 
-    print_orderbook(orderbook)
+  print_orderbook(orderbook)
+  {
+    console.log('Printing asks:');
+    let asks = orderbook.orderbook_manager.get_orderbook().asks;
+    let iterator = asks.iterate();
+    let curr = iterator.next();
+    while (!curr.done)
     {
-        console.log("Printing asks:");
-        let asks = orderbook.orderbook_manager.get_orderbook().asks;
-        let iterator = asks.iterate();
-        let curr = iterator.next();
-        while (!curr.done)
-        {
-            console.log('ask', curr.value.key, curr.value.value.size);
-            curr = iterator.next();
-        }
-        console.log("Printing bids:");
-        let bids = orderbook.orderbook_manager.get_orderbook().bids;
-        iterator = bids.iterate();
-        curr = iterator.next();
-        while (!curr.done)
-        {
-            console.log('bid', curr.value.key, curr.value.value.size);
-            curr = iterator.next();
-        }
+      console.log('ask', curr.value.key, curr.value.value.size);
+      curr = iterator.next();
     }
+    console.log('Printing bids:');
+    let bids = orderbook.orderbook_manager.get_orderbook().bids;
+    iterator = bids.iterate();
+    curr = iterator.next();
+    while (!curr.done)
+    {
+      console.log('bid', curr.value.key, curr.value.value.size);
+      curr = iterator.next();
+    }
+  }
 }
 
 export default bitstamp_orderbook;
