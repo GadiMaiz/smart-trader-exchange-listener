3e6c2ce2e646bd98c6b6b64bcd4e2e2ab0042e5d
Adi <adipiek@gmail.com>
2018-09-26 11:47:02 +0300

    Add delete test

--- src/orderbook/bitfinex_orderbook.spec.js
+++ src/orderbook/bitfinex_orderbook.spec.js
@@ -55,8 +55,30 @@ describe('Stub tests', () => {
         expect(orders).to.deep.equals(expected_orders);
     });
 
+    it('handle_data_message should delete order', () => {
+      const raw_message = [5674, [13, 0, 0]];
+      sinon.stub(bitfinexOrderbook.orderbook_manager, 'delete_order').callsFake((order) => {var orderIndex = orders.indexOf(orders.find(o => o.exchange_id === order.exchange_id));
+                                                                                            orders[orderIndex] = orders[0];
+                                                                                            orders.shift();
+                                                                                          });
+      const expected_orders = [
+        {price: 6982, size: 0.56, type: 'asks', exchange_id: '2', source: 'Bitfinex'},
+        {price: 6976, size: 0.25, type: 'bids', exchange_id: '3', source: 'Bitfinex'},
+        {price: 6995, size: 0.89, type: 'asks', exchange_id: '4', source: 'Bitfinex'},
+        {price: 6990, size: 0.7, type: 'asks', exchange_id: '5', source: 'Bitfinex'},
+        {price: 6975, size: 0.45, type: 'bids', exchange_id: '6', source: 'Bitfinex'},
+        {price: 6962, size: 0.83, type: 'bids', exchange_id: '7', source: 'Bitfinex'},
+        {price: 6960, size: 0.2, type: 'bids', exchange_id: '8', source: 'Bitfinex'},
+        {price: 6989, size: 0.6, type: 'asks', exchange_id: '9', source: 'Bitfinex'},
+        {price: 6988, size: 0.76, type: 'asks', exchange_id: '10', source: 'Bitfinex'},
+        {price: 6981, size: 0.7, type: 'asks', exchange_id: '11', source: 'Bitfinex'},
+        {price: 6981, size: 0.6, type: 'asks', exchange_id: '12', source: 'Bitfinex'},
+        {price: 6978, size: 0.32, type: 'asks', exchange_id: '1', source: 'Bitfinex'}];
 
-
+      bitfinexOrderbook.handle_data_message(raw_message);
+      expect(orders).to.deep.equals(expected_orders);
+    });  
+      
   });
 
 });
