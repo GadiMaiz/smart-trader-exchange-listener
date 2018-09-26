f80b78a84966cdf5e10ad84e8f2761aee84264b7
Adi <adipiek@gmail.com>
2018-09-26 15:46:15 +0300

    Add test to change existing order

--- src/orderbook/bitfinex_orderbook.spec.js
+++ src/orderbook/bitfinex_orderbook.spec.js
@@ -6,6 +6,19 @@ const sinon = api;
 var listener_stub = sinon.stub();
 var bitfinexOrderbook = new bitfinex_orderbook(listener_stub);
 let orders = [];
+let expected_orders = [{price: 6978, size: 0.32, type: 'asks', exchange_id: '1', source: 'Bitfinex'},
+        {price: 6982, size: 0.56, type: 'asks', exchange_id: '2', source: 'Bitfinex'},
+        {price: 6976, size: 0.25, type: 'bids', exchange_id: '3', source: 'Bitfinex'},
+        {price: 6995, size: 0.89, type: 'asks', exchange_id: '4', source: 'Bitfinex'},
+        {price: 6990, size: 0.7, type: 'asks', exchange_id: '5', source: 'Bitfinex'},
+        {price: 6975, size: 0.45, type: 'bids', exchange_id: '6', source: 'Bitfinex'},
+        {price: 6962, size: 0.83, type: 'bids', exchange_id: '7', source: 'Bitfinex'},
+        {price: 6960, size: 0.2, type: 'bids', exchange_id: '8', source: 'Bitfinex'},
+        {price: 6989, size: 0.6, type: 'asks', exchange_id: '9', source: 'Bitfinex'},
+        {price: 6988, size: 0.76, type: 'asks', exchange_id: '10', source: 'Bitfinex'},
+        {price: 6981, size: 0.7, type: 'asks', exchange_id: '11', source: 'Bitfinex'},
+        {price: 6981, size: 0.6, type: 'asks', exchange_id: '12', source: 'Bitfinex'},
+        {price: 6650, size: 0.3, type: 'bids', exchange_id: '13', source: 'Bitfinex'}];
 
 describe('Stub tests', () => {
 
@@ -36,48 +49,40 @@ describe('Stub tests', () => {
         const raw_snapshot = [5674, [[1, 6978, -0.32],[2, 6982, -0.56],[3, 6976, 0.25],[4, 6995, -0.89],[5, 6990, -0.7],
         [6, 6975, 0.45],[7, 6962, 0.83],[8, 6960, 0.2],[9, 6989, -0.6],[10, 6988, -0.76],[11, 6981, -0.7],[12, 6981, -0.6],[13, 6650, 0.3]]];
         sinon.stub(bitfinexOrderbook.orderbook_manager, 'add_order').callsFake((order) => orders.push(order));
-        const expected_orders = [{price: 6978, size: 0.32, type: 'asks', exchange_id: '1', source: 'Bitfinex'},
-        {price: 6982, size: 0.56, type: 'asks', exchange_id: '2', source: 'Bitfinex'},
-        {price: 6976, size: 0.25, type: 'bids', exchange_id: '3', source: 'Bitfinex'},
-        {price: 6995, size: 0.89, type: 'asks', exchange_id: '4', source: 'Bitfinex'},
-        {price: 6990, size: 0.7, type: 'asks', exchange_id: '5', source: 'Bitfinex'},
-        {price: 6975, size: 0.45, type: 'bids', exchange_id: '6', source: 'Bitfinex'},
-        {price: 6962, size: 0.83, type: 'bids', exchange_id: '7', source: 'Bitfinex'},
-        {price: 6960, size: 0.2, type: 'bids', exchange_id: '8', source: 'Bitfinex'},
-        {price: 6989, size: 0.6, type: 'asks', exchange_id: '9', source: 'Bitfinex'},
-        {price: 6988, size: 0.76, type: 'asks', exchange_id: '10', source: 'Bitfinex'},
-        {price: 6981, size: 0.7, type: 'asks', exchange_id: '11', source: 'Bitfinex'},
-        {price: 6981, size: 0.6, type: 'asks', exchange_id: '12', source: 'Bitfinex'},
-        {price: 6650, size: 0.3, type: 'bids', exchange_id: '13', source: 'Bitfinex'}];
 
         bitfinexOrderbook.handle_data_message(raw_snapshot);
         
-        expect(orders).to.deep.equals(expected_orders);
+        expect(orders).to.have.deep.members(expected_orders);
     });
 
     it('handle_data_message should delete order', () => {
       const raw_message = [5674, [13, 0, 0]];
-      sinon.stub(bitfinexOrderbook.orderbook_manager, 'delete_order').callsFake((order) => {var orderIndex = orders.indexOf(orders.find(o => o.exchange_id === order.exchange_id));
-                                                                                            orders[orderIndex] = orders[0];
-                                                                                            orders.shift();
-                                                                                          });
-      const expected_orders = [
-        {price: 6982, size: 0.56, type: 'asks', exchange_id: '2', source: 'Bitfinex'},
-        {price: 6976, size: 0.25, type: 'bids', exchange_id: '3', source: 'Bitfinex'},
-        {price: 6995, size: 0.89, type: 'asks', exchange_id: '4', source: 'Bitfinex'},
-        {price: 6990, size: 0.7, type: 'asks', exchange_id: '5', source: 'Bitfinex'},
-        {price: 6975, size: 0.45, type: 'bids', exchange_id: '6', source: 'Bitfinex'},
-        {price: 6962, size: 0.83, type: 'bids', exchange_id: '7', source: 'Bitfinex'},
-        {price: 6960, size: 0.2, type: 'bids', exchange_id: '8', source: 'Bitfinex'},
-        {price: 6989, size: 0.6, type: 'asks', exchange_id: '9', source: 'Bitfinex'},
-        {price: 6988, size: 0.76, type: 'asks', exchange_id: '10', source: 'Bitfinex'},
-        {price: 6981, size: 0.7, type: 'asks', exchange_id: '11', source: 'Bitfinex'},
-        {price: 6981, size: 0.6, type: 'asks', exchange_id: '12', source: 'Bitfinex'},
-        {price: 6978, size: 0.32, type: 'asks', exchange_id: '1', source: 'Bitfinex'}];
+      sinon.stub(bitfinexOrderbook.orderbook_manager, 'delete_order').callsFake((order) => {orders.pop()});
 
       bitfinexOrderbook.handle_data_message(raw_message);
-      expect(orders).to.deep.equals(expected_orders);
-    });  
+      expect(orders).to.have.deep.members(expected_orders.slice(0, -1));
+    });
+    
+    it('handle_data_message should add order', () => {
+      const raw_message = [5674, [13, 6650, 0.3]];
+      sinon.stub(bitfinexOrderbook, 'order_exists').onCall(0).returns(false).onCall(1).returns(true);
+
+      bitfinexOrderbook.handle_data_message(raw_message);
+      expect(orders).to.have.deep.members(expected_orders);
+    });
+
+    it('handle_data_message should change order', () => {
+      const raw_message = [5674, [13, 6750, 0.5]];
+      // sinon.stub(bitfinexOrderbook, 'order_exists').callsFake((order) => true);
+      sinon.stub(bitfinexOrderbook.orderbook_manager, 'change_order').callsFake((order) => { orders[12].price = raw_message[1][1];
+        orders[12].size = raw_message[1][2]; 
+      });
+
+      bitfinexOrderbook.handle_data_message(raw_message);
+      expected_orders[12].price = 6750;
+      expected_orders[12].size = 0.5;
+      expect(orders).to.have.deep.members(expected_orders);
+    });
       
   });
 
